7358f345d85f27e0705c71ad3ae13d4509f2bfe3
Varun Kumar <varunatt95@gmail.com>
2020-02-23 13:53:51 +0530

    Battery and Display new specification added

--- src/com/trainingbasket/fp/beans/Battery.java
+++ src/com/trainingbasket/fp/beans/Battery.java
@@ -2,18 +2,25 @@ package com.trainingbasket.fp.beans;
 
 public class Battery {
 
-    String backuptime;
+    private String backuptime;
+    private String chargingtime;
+    private String internalresistance;
+    private float cost;
 
-
-    public Battery(String backuptime) {
+    public Battery(String backuptime, String chargingtime, String internalresistance, float cost) {
         this.backuptime = backuptime;
+        this.chargingtime = chargingtime;
+        this.internalresistance = internalresistance;
+        this.cost = cost;
     }
 
     @Override
     public String toString() {
         return "Battery{" +
                 "backuptime='" + backuptime + '\'' +
+                ", chargingtime='" + chargingtime + '\'' +
+                ", internalresistance='" + internalresistance + '\'' +
+                ", cost=" + cost +
                 '}';
     }
-
 }
--- src/com/trainingbasket/fp/beans/Display.java
+++ src/com/trainingbasket/fp/beans/Display.java
@@ -1,17 +1,28 @@
 package com.trainingbasket.fp.beans;
 
 public class Display {
-    public Display(String size) {
+    private String size;
+    private String paneltype;
+    private String resolution;
+    private String ppi;
+    private float cost;
+
+    public Display(String size, String paneltype, String resolution, String ppi, float cost) {
         this.size = size;
+        this.paneltype = paneltype;
+        this.resolution = resolution;
+        this.ppi = ppi;
+        this.cost = cost;
     }
 
     @Override
     public String toString() {
         return "Display{" +
                 "size='" + size + '\'' +
+                ", paneltype='" + paneltype + '\'' +
+                ", resolution='" + resolution + '\'' +
+                ", ppi='" + ppi + '\'' +
+                ", cost=" + cost +
                 '}';
     }
-
-    String size;
-
 }
